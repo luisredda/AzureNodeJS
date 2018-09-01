1a15120e6f5cc3b15d2f51e196c7c86718d18468
Luis Redda <luis.redda@gmail.com>
2018-09-01 14:00:02 -0300

    Adjustments

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var os = require("os");
-var hostname = os.hostname();
-
-require("appdynamics").profile({
-    controllerHostName: 'lr45controllercent-lrdevopslab-leoo1lfg.srv.ravcloud.com',
-    controllerPort: 8090,
-      accountName: 'customer1',
-     accountAccessKey: '68e1e626-2bcc-491d-b888-b8688aee29ec',
-     applicationName: 'NodeJS_API',
-     tierName: 'AzureAppService',
-     nodeName: 'process',
-     libagent:true
- });
-
 var http = require('http');
 
-http.createServer(function (req, res) {
+require("appdynamics").profile({
+    controllerHostName: 'controllerurl.com',
+    controllerPort: 8090,
+    accountName: 'customer1',
+    accountAccessKey: '13e858d9',
+    applicationName: 'NodeJS_API',
+    tierName: 'AzureAppService',
+    nodeName: 'NodeProcess',
+    reuseNode: true,
+    reuseNodePrefix: "app.test",
+    libagent:true
+ });
 
-  for(i=0;i < 1000000000; i++) {
-    i+1;
-  }
+var server = http.createServer(function(request, response) {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(5000);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("AppDynamics the Best!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
